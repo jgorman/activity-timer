734846cea97cd5a3adad322baed74d28d7312f93
John Gorman <johngorman2@gmail.com>
2019-11-08 07:29:16 -0400

    Bootstrap working.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,4 +14,17 @@ require("channels")
 // If jQuery is not already registered in window.jQuery do it here.
 window.jQuery = window.$ = require('jquery')
 
+require('bootstrap')
 
+/*
+ * App setup.
+ */
+
+// https://github.com/webpack/docs/wiki/context
+const requireAll = context => context.keys().map(context)
+
+requireAll(require.context('../javascript', false, /\.(js|jsx)$/i))
+
+require('../stylesheets')
+
+const images = require.context('../images', false, /\.(png|svg|jpg)$/i)
