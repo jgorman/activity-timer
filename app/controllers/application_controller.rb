f37a5e6a18dd43dc553dabb68a43a5a96f86b9fd
John Gorman <johngorman2@gmail.com>
2019-11-13 08:40:37 -0400

    Devise registration add user first_name last_name.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,15 @@
-class ApplicationController < ActionController::Base; end
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :email, :password)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :current_password)
+    end
+  end
+end
