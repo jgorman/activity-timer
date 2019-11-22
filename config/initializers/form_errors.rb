29d9ac5a26e0df14634a04ba77b4f7133f79a2c7
John Gorman <johngorman2@gmail.com>
2019-11-22 16:31:29 -0400

    Clearer config/initializers/form_errors.rb

--- config/initializers/form_errors.rb
+++ config/initializers/form_errors.rb
@@ -1,25 +1,31 @@
-# Default
-# Proc.new { |html_tag, instance| "<div class=\"field_with_errors\">#{html_tag}</div>".html_safe }
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
-  fragment = Nokogiri::HTML.fragment(html_tag)
-  field = fragment.at('input,select,textarea')
+# Insert model validation error messages after the input elements.
+#
+# Add this file to config/initializers/form_errors.rb
+#
+# Overrides the default function:
+#
+# Proc.new { |html_tag, instance_tag|
+#   "<div class=\"field_with_errors\">#{html_tag}</div>".html_safe
+# }
 
-  model = instance_tag.object
-  field_name = instance_tag.instance_variable_get(:@method_name)
-  field_title = field_name.titleize
-  errors = model.errors[field_name]
-  field_errors = errors.map { |error| "#{field_title} #{error}" }.join(', ')
+ActionView::Base.field_error_proc =
+  Proc.new do |html_tag, instance_tag|
+    fragment = Nokogiri::HTML.fragment(html_tag)
+    field = fragment.at('input,select,textarea')
 
-  html = if field
-           field['class'] = "#{field['class']} invalid"
-           html = <<-HTML
-			#{fragment.to_s}
-			<p class="error">#{field_errors}</p>
-           HTML
-           html
-         else
-           html_tag
-         end
+    model = instance_tag.object
+    field_name = instance_tag.instance_variable_get(:@method_name)
+    field_title = field_name.titleize
+    errors = model.errors[field_name]
+    field_errors = errors.map { |error| "#{field_title} #{error}" }.join(', ')
 
-  html.html_safe
-end
+    html =
+      if field
+        field['class'] = "#{field['class']} invalid"
+        "#{fragment.to_s} <span class=\"error\"><br>#{field_errors}</span>"
+      else
+        html_tag
+      end
+
+    html.html_safe
+  end
