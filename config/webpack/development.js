e0b2eb1a94f1657c33136a0f7682fe22c9a18bca
John Gorman <johngorman2@gmail.com>
2020-06-26 21:03:53 -0300

    Webpack instrumentation.

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development"
 
 const environment = require("./environment")
 
-module.exports = environment.toWebpackConfig()
+// Examine webpack-dev-server output.
+const webpackConfig = environment.toWebpackConfig()
+// console.log(webpackConfig)
+// console.dir( webpackConfig, { depth: null })
+
+// Inspect the webpackConfig
+// debugger
+
+module.exports = webpackConfig
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
 const { environment } = require("@rails/webpacker")
 
+/*
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
+enviroment.plugins.prepend("BundleAnalyzerPlugin", new BundleAnalyzerPlugin())
+enviroment.splitChunks()
+*/
+
 module.exports = environment
