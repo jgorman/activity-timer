d1a887fe7a4bd89b1d19d649084b52c2a4734c01
John Gorman <johngorman2@gmail.com>
2020-07-27 05:20:40 -0300

    Set up ReCAPTCHA for testing.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-config = Rails.application.credentials.config
-ENV["RECAPTCHA_SITE_KEY"] = config.dig(:recaptcha, :site_key)
-ENV["RECAPTCHA_SECRET_KEY"] = config.dig(:recaptcha, :secret_key)
+if Rails.env.test?
+  ENV["RECAPTCHA_SITE_KEY"] = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+  ENV["RECAPTCHA_SECRET_KEY"] = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+else
+  config = Rails.application.credentials.config
+  ENV["RECAPTCHA_SITE_KEY"] = config.dig(:recaptcha, :site_key)
+  ENV["RECAPTCHA_SECRET_KEY"] = config.dig(:recaptcha, :secret_key)
+end
 
